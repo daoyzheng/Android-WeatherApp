74101197bda0b59622de6f5d05dbd8752a95018a
Dao Zheng <daoyzheng@gmail.com>
2019-05-02 11:32:58 -0600

    Add AsyncTask method

---
+++ app/src/main/java/com/example/weatherapp/WeatherAppAsyncTask.java
@@ -0,0 +1,15 @@
+package com.example.weatherapp;
+
+import android.os.AsyncTask;
+
+public class WeatherAppAsyncTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
